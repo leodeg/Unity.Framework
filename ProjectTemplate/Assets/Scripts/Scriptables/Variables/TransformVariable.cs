2ce1923ecd386bf56bf0f15f5cea6b2a480e09ef
LeoDeg <rdkdvv@gmail.com>
2019-04-14 13:45:51 +0300

    Initialize the project template

---
+++ ProjectTemplate/Assets/Scripts/Scriptables/Variables/TransformVariable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StateObject
+{
+    [CreateAssetMenu(menuName = "Variables/Transform")]
+    public class TransformVariable : ScriptableObject
+    {
+        public Transform transform;
+
+        public void Set(Transform v)
+        {
+            transform = v;
+        }
+
+        public void Set(TransformVariable v)
+        {
+            transform = v.transform;
+        }
+
+        public void Clear()
+        {
+            transform = null;
+        }
+    }
+}
