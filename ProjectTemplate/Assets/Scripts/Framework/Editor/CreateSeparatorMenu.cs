12730c54f1a6bbc82fcf3dbdce9830119ce2f938
LeoDeg <rdkdvv@gmail.com>
2019-05-09 20:11:17 +0300

    ADDED: hierarchy separator, grouper of game objects tool, action to state action adapter

---
+++ ProjectTemplate/Assets/Scripts/Framework/Editor/CreateSeparatorMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace LeoDeg.Editor
+{
+    public class CreateSeparatorMenu : MonoBehaviour
+    {
+        [MenuItem ("LeoDeg/Hierarchy Separator %#&s")]
+        public static void CreateHierarchySeparator ()
+        {
+            GameObject separator = new GameObject ();
+            separator.name = "-----------------";
+        }
+    }
+}
---
+++ ProjectTemplate/Assets/Scripts/Framework/Editor/GroupObjectsMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace LeoDeg.Editor
+{
+    public class GroupObjectsMenu : MonoBehaviour
+    {
+
+        [MenuItem ("LeoDeg/Group Objects %#&d")]
+        public static void LaunchGroupObjects ()
+        {
+            GroupObjectsEditorWindow.OpenWindow ();
+        }
+    }
+}
